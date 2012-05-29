08c0c8718852710940ecd5670be74c5f279ec5ef
Arvind Doraiswamy <arvind.doraiswamy@gmail.com>
2012-05-29 20:16:30 +0530

    Responses now written to XML

--- fuzzer_scripts/fuzz_get_folder_structure_library.py
+++ fuzzer_scripts/fuzz_get_folder_structure_library.py
@@ -3,8 +3,10 @@ import sys
 import os
 import re
 import datetime
+from lxml import etree
 
-payloads=[]
+payloads=[]; responses={}
+
 url='http://50.16.28.105:8000/aspx_pages/TM_WebServices.asmx?WSDL'
 client = suds.client.Client(url)
 d = dict(http='127.0.0.1:8080')
@@ -16,17 +18,15 @@ def get_response_path():
 
 def get_folderstructure_library(list_payload):
   path=get_response_path()
-  f=open(path,'w')
   for i in list_payload:
-    f.write('\n\n'+'='*60+str(i)+'='*60+'\n\n')
     try:
       a=client.service.GetFolderStructure_Library(str(i))
-      f.write(str(a)[0:300])
+      responses[i]=str(a)[0:300]
     except Exception:
       exc_type, exc_value = sys.exc_info()[:2]
-      f.write(str(exc_value)[0:300])
+      responses[i]=str(exc_value)[0:300]
       pass
-  f.close()
+  return responses
 
 def get_payloads():
   f=open('guid_payloads','r')
@@ -36,8 +36,20 @@ def get_payloads():
   f.close()
   return payloads
 
+def generate_xml(responses):
+  root = etree.Element("Method", name=met)
+  for key in responses.keys():
+    payload = etree.SubElement(root,"Payload")
+    payload.text = key
+    resp = etree.SubElement(payload,"Response")
+    resp.text = responses[key]
+
+  t1 = etree.ElementTree(root)
+  t1.write('resp.xml', pretty_print=True)
+
 def main():
   list_payload=get_payloads()
-  get_folderstructure_library(list_payload)
+  responses=get_folderstructure_library(list_payload)
+  report=generate_xml(responses)
 
 main()
